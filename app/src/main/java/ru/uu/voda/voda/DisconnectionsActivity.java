b19adab23de6ee15e27b7262c3f3696ef87935d2
Alexander Morozov <c-a-h-e-k2006@yandex.ru>
2017-09-27 23:53:30 +0500

    Датапикер в отключениях Добавлен выбор даты в отключениях

--- app/src/main/java/ru/uu/voda/voda/DisconnectionsActivity.java
+++ app/src/main/java/ru/uu/voda/voda/DisconnectionsActivity.java
@@ -1,8 +1,13 @@
 package ru.uu.voda.voda;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class DisconnectionsActivity extends AppCompatActivity {
@@ -13,8 +18,19 @@ public class DisconnectionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_disconnections);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Use the current date as the default date
+        final Calendar c = Calendar.getInstance();
+        final int year = c.get(Calendar.YEAR);
+        final int month = c.get(Calendar.MONTH);
+        final int day = c.get(Calendar.DAY_OF_MONTH);
+        ((TextView) findViewById(R.id.calendartext)).setText(day + " " + getResources().getStringArray(R.array.month)[month] + " " + year);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
 }
